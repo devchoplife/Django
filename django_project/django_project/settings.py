727fd47325457d9dfed04544bf511a71d9d4041d
Durojaiye <michael.durojaiye@savethechildren.org>
2022-04-27 19:33:12 +0100

    Update settings file to migrate the app

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world.apps.Hello_worldConfig'
+    'hello_world.apps.HelloWorldConfig'
 ]
 
 MIDDLEWARE = [
