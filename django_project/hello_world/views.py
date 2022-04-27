28fb208ccb8d9176d88f8736bcc84ddb117cc77e
Durojaiye <michael.durojaiye@savethechildren.org>
2022-04-27 17:21:33 +0100

    Add view for hello world app

--- django_project/hello_world/views.py
+++ django_project/hello_world/views.py
@@ -1,3 +1,10 @@
+"""
+Django views are Python functions that takes http requests and returns http response, like HTML documents.
+A web page that uses Django is full of views with different tasks and missions.
+Views are usually put in a file called views.py located on your app's folder
+"""
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello World!")
