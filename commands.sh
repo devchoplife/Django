0d8f6d3a10e4b0ca4fbcff4a331d7b25dab609d9
devchoplife <dritedurojaiye@gmail.com>
2022-05-02 14:57:05 +0100

    Populate commands file

--- commands.sh
+++ commands.sh
@@ -1,1 +1,11 @@
 # Useful commands when working with Django
+
+python3 -m venv <virtual-environment-name> # Create virtual environment 
+python3 -m pip install django # install django on virtual environment
+django-admin --version # Check sjango version/to also check if django is installed 
+pip freeze # This can also show you if django is installed 
+django-amin startproject <project-name> # Create a new django project
+python3 <project-dir>/manage.py runserver # start the server so you can see your live website 
+python3 <project-dir>/manage.py startapp <app-name> # create a new django application
+python3 <project-dir>/manage.py makemigrations <app-name> # Create migrations after making changes to the table or database
+python3 <project-dir>/manage.py migrate # make the migration
